d41f7a801a70606c495914e0e4bfa637eefbbd50
jod35 <jodestrevin@gmail.com>
2020-07-19 22:54:40 -0700

    set up a login manager and bcrypt for password hashing

--- snap/__init__.py
+++ snap/__init__.py
@@ -1,11 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from .config import DevConfig
 from snap.users.views import user_bp
 
 app=Flask(__name__)
 app.config.from_object(DevConfig)
+
+#init db
 db=SQLAlchemy(app)
 
+#init login manager
+login_manager=LoginManager()
+login_manager.init_app(app)
+
+#init bcrypt (password hashing library)
+bcrypt=Bcrypt()
+
+bcrypt.init_app(app)
+
+
+
+from snap.users.models import User as user_model
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return user_model.query.get(int(user_id))
 
 app.register_blueprint(user_bp,url_prefix='/users')
